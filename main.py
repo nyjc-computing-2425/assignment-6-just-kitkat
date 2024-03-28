4eab14a45d793247c70ab0beaf3b91f85fe3d571
justkitkat <121209669+just-kitkat@users.noreply.github.com>
2024-03-28 07:04:13 +0000

    Submission

--- main.py
+++ main.py
@@ -4,17 +4,56 @@ import time
 
 # Part 1
 def clock(n):
-    # Your code here
-    pass
+    """
+    Output the time and update it once every second, for n number of seconds.
 
+    Arguements:
+    n: int - number of times to update the time
+
+    Return:
+    None
+    """
+    for i in range(n):
+        time_data = datetime.now()
+        cur_time = time_data.strftime("%H:%M:%S")
+        print(cur_time, end="\r")
+        time.sleep(1)
 
 # Part 2
 def lcm(a, b):
-    # Your code here
-    pass
+    """
+    Calculates the lowest common multiple of a and b
 
+    Arguments:
+    a: int
+    b: int
+
+    Return:
+    int -- the LCM of a and b
+    """
+    original_a, original_b = a, b
+    while True:
+        if a == b:
+            return a
+        if a < b:
+            a += original_a
+        else:
+            b += original_b
 
 # Part 3
 def gcf(a, b):
-    # Your code here
-    pass
+    """
+    Calculates the greatest common factor of a and b
+
+    Arguments:
+    a: int
+    b: int
+
+    Return:
+    int - GCF of a and b
+    """
+    while True:
+        if b == 0:
+            return a
+        r = a % b
+        a, b = b, r
